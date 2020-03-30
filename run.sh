2e99daa0f2ab20a4caed55e2ead839eeb911a1e3
Your Name <you@example.com>
2020-03-30 16:23:38 +0000

    upload

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@ for i in git ansible
 do
 ( rpm -q $i ) || yum install $i -y 
 done
-ansible-playbook -i localhost -U https://github.com/abi25feb/docker.git docker.yml 
+ansible-pull -i localhost -U https://github.com/abi25feb/docker.git docker.yml 
 
